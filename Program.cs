bb3d71415af7afb8fb90ac407c100e0fe673e025
copeitu <cope@itu.dk>
2023-08-31 15:21:21 +0200

    Flere ændringer

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿List<string> cheeps = new() { "Hello, BDSA students!"
+, "Welcome to the course!"
+, "I hope you had a good summer." };
 
-
-       
+foreach (var cheep in cheeps)
+{
+    Console.WriteLine(cheep);
+    Thread.Sleep(1000);
+}
