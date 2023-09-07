9afe81f48d63debd9dd5125cb36c1ebd4a27ad71
copeitu <cope@itu.dk>
2023-09-07 14:41:14 +0200

    Refactor main to print CLI arguments.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,80 @@
-﻿List<string> cheeps = new() { "Hello, BDSA students!"
-, "Welcome to the course!"
-, "I hope you had a good summer." };
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
 
-foreach (var cheep in cheeps)
+using Microsoft.VisualBasic;
+
+namespace MyProject;
+class Program
 {
-    Console.WriteLine(cheep);
-    Thread.Sleep(1000);
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            if (args[0].ToLower() == "chirp")
+            {
+                Skriv(args[1]);
+            }
+            else if (args[0].ToLower() == "read")
+            {
+                Laes(args[1]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No arguments");
+        }
+
+    }
+
+    public static void Laes(string a)
+    {        // Chirp.CLI cheep "Welcome to the course!"
+
+        string path = @"C:\Users\cope\OneDrive - ITU\Desktop\ChirpCLI\chirp_cli_db.csv";
+        int i = int.Parse(a);
+        int j = 0;
+        StreamReader sr = new(path);
+        string[] row = new string[5];
+
+        string? line;
+        while ((line = sr.ReadLine()) != null && j < i)
+        {
+            row = line.Split(',');
+
+            Console.WriteLine(line);
+            j = j + 1;
+        }
+
+        sr.Close();
+    }
+    public static void Skriv(string a)
+    {
+        // Chirp.CLI cheep "Welcome to the course!"
+
+        string path = @"C:\Users\cope\OneDrive - ITU\Desktop\ChirpCLI\chirp_cli_db.csv";
+
+        using (StreamWriter writer = File.AppendText(path))
+        {
+            //int t = DateTimeOffset.Now.FromUnixTimeSeconds.ToString();
+
+            //string t = DateTimeOffset.Now.Offset.TotalMicroseconds.ToString(); // Rigtigt format
+
+            DateTime t = DateTime.Now;
+
+            //t = t.Substring(0, t.Length - 2);
+            //t =  cast  t.Substring(0, t.Length - 2);
+
+            string newRow = "\"" + Environment.UserName + ",\"\"" + a + "\"\"," + t.ToString() + "\"";
+            string printRow = Environment.UserName + ", " + t.ToString() + ", " + a;
+
+            Console.WriteLine(printRow);
+
+            writer.WriteLine(newRow);
+
+            writer.Close();
+        }
+
+    }
 }
+
